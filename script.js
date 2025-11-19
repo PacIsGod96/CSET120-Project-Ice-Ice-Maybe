525b5faf093225c0649fce6e7f0ea0c87275118a
Collin <williamsck233@gmail.com>
2025-11-18 22:48:44 -0500

    fixedjs and css to have the sign/login animations to work

--- script.js
+++ script.js
@@ -44,36 +44,39 @@ function passwordToggle(id, link){
 document.addEventListener("DOMContentLoaded", () =>{
     let loginSignUpBtn = document.querySelector(".login-SignUp-Btn")
     let modal = document.getElementById("modal")
-    let xBtn = document.querySelector("xIcon-Btn")
+    let xBtns = document.querySelectorAll(".xIcon-Btn")
     let loginPanel = document.querySelector(".login")
     let signUpPanel = document.querySelector(".signUp")
 
     loginSignUpBtn.addEventListener("click", () => {
         modal.style.display = "flex"
-        loginPanel.classList.remove("active")
+        loginPanel.classList.add("active")
         signUpPanel.classList.remove("active")
     })
-
-    xBtn.addEventListener("click", () => {
-        modal.style.display = "none"
+    xBtns.forEach(xBtn => {
+        xBtn.addEventListener("click", () => {
+            modal.style.display = "none"
+            loginPanel.classList.remove("active")
+            signUpPanel.classList.remove("active")
+        })
     })
 
 
-    let switchToSignUpLink = loginPanel.querySelector("a")
+    let switchToSignUpLink = document.getElementById("goToSignUp")
     if(switchToSignUpLink){
         switchToSignUpLink.addEventListener("click", (e) => {
             e.preventDefault()
-            loginPanel.classList.add("active")
-            signUpPanel.classList.remove("active")
+            loginPanel.classList.remove("active")
+            signUpPanel.classList.add("active")
         })
     }
 
-    let switchToLoginLink = signUpPanel.querySelector("button")
+    let switchToLoginLink = document.querySelector(".SignUpBtn")
     if(switchToLoginLink){
         switchToLoginLink.addEventListener("click", (e) => {
             e.preventDefault()
-            loginPanel.remove("active")
-            signUpPanel.add("active")
+            loginPanel.classList.add("active")
+            signUpPanel.classList.remove("active")
         })
     }
 
